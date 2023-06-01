10177810362af1366693ec95042b795650351de4
simon greenwood <simonrobertgreenwood@gmail.com>
2023-06-01 18:14:01 +0100

    added express app with typescript

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+// set up an express server in typescript
+import express from "express";
+import { PORT } from "./utils/config";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
---
+++ backend/utils/config.ts
@@ -0,0 +1,3 @@
+require("dotenv").config();
+const PORT: number = Number(process.env.PORT);
+export { PORT };
