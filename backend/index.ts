1fd76544af18509723ff1a6bfbc0fc26b798025f
simon greenwood <simonrobertgreenwood@gmail.com>
2023-06-03 19:02:47 +0100

    added the frontend folder

--- backend/index.ts
+++ backend/index.ts
@@ -7,6 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -1,3 +1,3 @@
 require("dotenv").config();
-const PORT: number = Number(process.env.PORT);
+const PORT: number = Number(process.env.PORT) || 3003;
 export { PORT };
